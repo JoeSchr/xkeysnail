607396b696cd4b641bb8e0db4dd0a7317325f352
JoeSchr <JoeSchr@users.noreply.github.com>
2020-10-28 22:40:18 +0100

    Fix missing '}'

--- example/config.py
+++ example/config.py
@@ -24,7 +24,7 @@ define_multipurpose_modmap(
     {Key.ENTER: [Key.ENTER, Key.RIGHT_CTRL]}
 
     # Capslock is escape when pressed and released. Control when held down.
-    # {Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL]
+    # {Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL]}
     # To use this example, you can't remap capslock with define_modmap.
 )
 
